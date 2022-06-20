4c09749820b1827943067db4172ed78e5238c761
MBochi <107650864+MBochi@users.noreply.github.com>
2022-06-20 14:24:41 +0200

    Projektile und Schießen

--- 2DGame/Assets/Scripts/PlayerMovement.cs
+++ 2DGame/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed = 10f;
-    Vector2 movement;
     public Rigidbody2D rb;
+    public Camera cam;
+    Vector2 movement;
+    Vector2 mousePos;
+
 
     private void Update() {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
     private void FixedUpdate() {
         rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg -90f;
+        rb.rotation = angle;
     }
 }
